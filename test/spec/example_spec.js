93330465b6c7c510d587fa119711760c9b92c164
Alex Chesters <alex.chesters@bbc.co.uk>
2017-01-25 08:31:03 +0000

    Add unit test

---
+++ test/spec/example_spec.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+
+import { expect } from 'chai'
+import { fromJS } from 'immutable'
+
+describe('Unit test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true)
+  })
+})
