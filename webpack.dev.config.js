e064b42f73180f25d2191a5e03868ddde8930c92
Alex Chesters <alex.chesters@bbc.co.uk>
2017-01-25 08:34:23 +0000

    Add webpack

---
+++ webpack.dev.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const config = require('./config/development')
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8081',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: 'bundle.[hash].js'
+  },
+  devServer: {
+    contentBase: './build',
+    hot: true,
+    historyApiFallback: true
+  },
+  watchOptions: {
+    poll: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin(config),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.hbs',
+      favicon: 'src/favicon.png',
+      dev: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const config = require('./config/production')
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin(config),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.hbs',
+      favicon: 'src/favicon.png',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
