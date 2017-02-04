af6a3ca6969baa054e57bba5cd52aeed74889f8e
Alex Chesters <alex.chesters@bbc.co.uk>
2017-02-04 19:32:55 +0000

    Don't use arrow functions

--- test/components/ExampleComponent.spec.jsx
+++ test/components/ExampleComponent.spec.jsx
@@ -6,13 +6,13 @@ import { expect } from 'chai'
 
 import ExampleComponent from '../../src/components/Example'
 
-describe('Example component', () => {
-  it('renders text', () => {
+describe('Example component', function () {
+  it('renders text', function () {
     const wrapper = shallow(<ExampleComponent text='foobar' />)
     expect(wrapper.find('h1')).to.have.length(1)
     expect(wrapper.text()).to.equal('foobar')
   })
-  it('renders default text if no text is given', () => {
+  it('renders default text if no text is given', function () {
     const wrapper = shallow(<ExampleComponent />)
     expect(wrapper.find('h1')).to.have.length(1)
     expect(wrapper.text()).to.equal('Hello, world!')
