d81f524ff2aeffa6ceb091ecc679fc4e98ee00f1
Alex Chesters <alex.chesters@bbc.co.uk>
2017-08-23 15:28:28 +0100

    Tweak format

--- src/routes/home/actions/random-string.js
+++ src/routes/home/actions/random-string.js
@@ -1,14 +1,14 @@
 import { SET_RANDOM_STRING } from '../action-types'
 
-export const setRandomString = string => {
+export const setRandomString = (payload) => {
   return {
     type: SET_RANDOM_STRING,
-    string
+    payload
   }
 }
 
-export const getRandomString = _ => {
-  return dispatch => {
+export const getRandomString = () => {
+  return (dispatch) => {
     window.setTimeout(_ => {
       return dispatch(setRandomString(Math.random().toString(36).substring(7)))
     }, 3000)
--- src/routes/home/reducers/random-string.js
+++ src/routes/home/reducers/random-string.js
@@ -3,7 +3,7 @@ import { SET_RANDOM_STRING } from '../action-types'
 export default (state = {}, action) => {
   switch (action.type) {
     case SET_RANDOM_STRING:
-      return { ...state, string: action.string }
+      return { ...state, string: action.payload }
     default:
       return state
   }
