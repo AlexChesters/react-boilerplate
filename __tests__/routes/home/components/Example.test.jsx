4b77c11aa59806bf1c5b06e04ac305b69daf3c62
Alex Chesters <AlexChesters@users.noreply.github.com>
2017-08-23 15:58:37 +0100

    Update Example.test.jsx

--- __tests__/routes/home/components/Example.test.jsx
+++ __tests__/routes/home/components/Example.test.jsx
@@ -8,7 +8,7 @@ import Example from '../../../../src/routes/home/components/Example'
 describe('Example', () => {
   it('renders correctly', () => {
     const tree = renderer.create(
-      <Example path={'/2017-04'} text='2017-04' />
+      <Example />
     ).toJSON()
     expect(tree).toMatchSnapshot()
   })
