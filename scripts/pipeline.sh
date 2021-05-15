ef49cb1428c42710f63a6dc72c3c7081179cc40f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-15 23:56:36 +0100

    update ci

--- scripts/pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-AWS_PROFILE=personal \
-  aws cloudformation deploy \
-  --template-file ci/codepipeline.yml \
-  --stack-name codepipeline-__name__ \
-  --capabilities CAPABILITY_IAM \
-  --region eu-west-1
